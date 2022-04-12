ee8cff1283659a957cbad26f3b46d77d29205504
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-12 11:47:15 +0300

    it will create the form for updating the user

--- vote/forms.py
+++ vote/forms.py
@@ -28,3 +28,9 @@ class RatingForm(forms.ModelForm):
         model = Rating
         fields = ['design', 'usability', 'content']
 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
