0310a1b8caafb1725999a1bc7c6e01c524afabc9
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-12 10:52:20 +0300

    it will create the Profile class

--- vote/models.py
+++ vote/models.py
@@ -5,4 +5,22 @@ from django.dispatch import receiver
 from phone_field import PhoneField
 
 # Create your models here.
-class User
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    bio = models.TextField(max_length=400, blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = models.ImageField(upload_to='images/',default='v1639327874/images/default_drurzc.jpg')
+    phone_number = PhoneField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
