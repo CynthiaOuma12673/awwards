79ed4fe4f5d83c1a7c70ac98b1dba4e374e0b39d
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-12 11:49:42 +0300

    it will add imports to the views file

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponseRedirect,Http404
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .forms import  UpdateUserForm, UpdateUserProfileForm, UserRegisterForm,PostForm,RatingForm
+from .models import Post, Profile, Rating
 # Create your views here.
