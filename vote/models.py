0ceb9b134fdaadd59bb07b7b1bd282390e600b86
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-12 10:54:43 +0300

    it will create the post class in models

--- vote/models.py
+++ vote/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from phone_field import PhoneField
 
 # Create your models here.
+# profile class
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     bio = models.TextField(max_length=400, blank=True)
@@ -24,3 +25,29 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+# post class
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=300)
+    url = models.URLField(max_length=400)
+    photo = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    upload_date = models.DateTimeField(auto_now_add=True, blank=True)
+    technologies_used = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
