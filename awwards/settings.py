4efe0d30d5fe37d2fb769a5d2aa0cb94e8949caf
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-12 20:58:25 +0300

    deployment

--- awwards/settings.py
+++ awwards/settings.py
@@ -15,9 +15,40 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
+    }
+# production
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +60,7 @@ SECRET_KEY = 'lb27z5$*)usc(ub&i5@n7%m^^&fc77)@@n*dp(+d499xm=24jw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,6 +87,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,6 +182,7 @@ cloudinary.config(
     api_key = "349849632571232", 
     api_secret = "CXsauSNf_MIPEHMQn4lsNrJv-Sc" 
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -166,3 +199,5 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
 
 ]
+
+django_heroku.settings(locals())
--- vote/models.py
+++ vote/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from phone_field import PhoneField
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 # profile class
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     bio = models.TextField(max_length=400, blank=True)
     name = models.CharField(blank=True, max_length=120)
-    profile_pic = models.ImageField(upload_to='images/',default='v1639327874/images/default_drurzc.jpg')
+    profile_pic = CloudinaryField('profile_pic')
     phone_number = PhoneField(max_length=15, blank=True)
     
     def __str__(self):
@@ -30,7 +30,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=300)
     url = models.URLField(max_length=400)
-    photo = models.ImageField(upload_to='images/')
+    photo = CloudinaryField('photo')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     upload_date = models.DateTimeField(auto_now_add=True, blank=True)
     technologies_used = models.CharField(max_length=200, blank=True)
